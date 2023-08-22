4123718a8b158f81a0895f9793baf24202a4cd84
zevik <zevik2001@gmail.com>
2023-08-22 11:51:31 +0300

    secons function

--- t1/ex.ts
+++ t1/ex.ts
@@ -6,3 +6,13 @@ const topNumber=(num1:number,num2:number):number=>{
     }
         return num2;
 }
+//2
+function printBigger (num1:number,num2:number):void{
+    num1 > num2 ?console.log (num1) : console.log(num2);
+}
+
+
+
+//console.log(topNumber(2,4))
+//printBigger(2,4)
+//console.log(typeNumber(5))
